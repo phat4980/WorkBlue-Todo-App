b37cd0da095792375d71aea70cb9f92afda3fac1
TPLouis <phat4980@gmail.com>
2022-11-22 02:23:30 +0700

    add loginActivity

--- app/src/main/java/tdtu/workblue/todoapplication/SplashActivity.java
+++ app/src/main/java/tdtu/workblue/todoapplication/SplashActivity.java
@@ -24,7 +24,7 @@ public class SplashActivity extends AppCompatActivity {
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                startActivity(new Intent(SplashActivity.this, LoginActivity.class));
                 finish();
             }
         }, 3000);
