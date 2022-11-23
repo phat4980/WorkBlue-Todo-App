d1769000f55bf75c32aa7d8072a83b3e20c971a5
TPLouis <phat4980@gmail.com>
2022-11-24 00:54:13 +0700

    edit login/signup

--- app/src/main/java/tdtu/workblue/todoapplication/RegisterActivity.java
+++ app/src/main/java/tdtu/workblue/todoapplication/RegisterActivity.java
@@ -9,9 +9,11 @@ import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
 import android.util.Patterns;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -30,6 +32,7 @@ public class RegisterActivity extends AppCompatActivity {
     TextInputLayout nameTextInputSU, emailTextInputSU, passwordTextInputSU;
     TextInputEditText nameEditTextSU, emailEditTextSU, passwordEditTextSU;
     Button btnSignUp;
+    ProgressBar progressBar;
     LinearLayout ToLogPage;
 
     @Override
@@ -41,6 +44,10 @@ public class RegisterActivity extends AppCompatActivity {
         actionBar.hide();
 
         initUi();
+        changeInProcess(false);
+        deleteErrorShowN();
+        deleteErrorShowE();
+        deleteErrorShowP();
         createUser();
         changeToLoginPage();
 
@@ -65,119 +72,40 @@ public class RegisterActivity extends AppCompatActivity {
         {
             nameTextInputSU.setError("Name is required");
         }
-        if(TextUtils.isEmpty(email))
-        {
-            emailTextInputSU.setError("Email is required");
+        else {
+            nameTextInputSU.setError(null);
         }
+
         if(!Patterns.EMAIL_ADDRESS.matcher(email).matches())
         {
             emailTextInputSU.setError("Email is invalid");
+        } else {
+            emailEditTextSU.setError(null);
         }
-        if(TextUtils.isEmpty(password))
-        {
-            passwordTextInputSU.setError("Password is required");
-        }
+
         if(password.length() <=8)
         {
             passwordTextInputSU.setError("Enter at least 8 characters");
         } else {
-            nameTextInputSU.setError(null);
-            emailEditTextSU.setError(null);
+            changeInProcess(true);
             passwordTextInputSU.setError(null);
             Auth.createUserWithEmailAndPassword(email, password)
                     .addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
                         @Override
                         public void onComplete(@NonNull Task<AuthResult> task) {
+                            changeInProcess(false);
                             if (task.isSuccessful()) {
-                                Toast.makeText(RegisterActivity.this, "Sign up successful", Toast.LENGTH_SHORT).show();
+                                Toast.makeText(RegisterActivity.this, "Sign up successful!", Toast.LENGTH_LONG).show();
+                                Toast.makeText(RegisterActivity.this, "Check your email to verify", Toast.LENGTH_LONG).show();
+                                Auth.getCurrentUser().sendEmailVerification();
                                 NavigateToLog();
                                 finishAffinity();
                             } else {
-                                Toast.makeText(RegisterActivity.this, "Authentication failed", Toast.LENGTH_SHORT).show();
+                                Toast.makeText(RegisterActivity.this, "Authentication failed", Toast.LENGTH_LONG).show();
                             }
                         }
                     });
         }
-
-                            /*String errorCode = ((FirebaseAuthException) task.getException()).getErrorCode();
-
-                            switch (errorCode) {
-
-                                case "ERROR_INVALID_CUSTOM_TOKEN":
-                                    Toast.makeText(RegisterActivity.this, "The custom token format is incorrect. Please check the documentation.", Toast.LENGTH_LONG).show();
-                                    break;
-
-                                case "ERROR_CUSTOM_TOKEN_MISMATCH":
-                                    Toast.makeText(RegisterActivity.this, "The custom token corresponds to a different audience.", Toast.LENGTH_LONG).show();
-                                    break;
-
-                                case "ERROR_INVALID_CREDENTIAL":
-                                    Toast.makeText(RegisterActivity.this, "The supplied auth credential is malformed or has expired.", Toast.LENGTH_LONG).show();
-                                    break;
-
-                                case "ERROR_INVALID_EMAIL":
-                                    Toast.makeText(RegisterActivity.this, "The email address is badly formatted.", Toast.LENGTH_LONG).show();
-                                    emailTextInputSU.setError("The email address is badly formatted.");
-                                    emailEditTextSU.requestFocus();
-                                    break;
-
-                                case "ERROR_WRONG_PASSWORD":
-                                    Toast.makeText(RegisterActivity.this, "The password is invalid or the user does not have a password.", Toast.LENGTH_LONG).show();
-                                    passwordTextInputSU.setError("password is incorrect ");
-                                    passwordEditTextSU.requestFocus();
-                                    passwordEditTextSU.setText("");
-                                    break;
-
-                                case "ERROR_USER_MISMATCH":
-                                    Toast.makeText(RegisterActivity.this, "The supplied credentials do not correspond to the previously signed in user.", Toast.LENGTH_LONG).show();
-                                    break;
-
-                                case "ERROR_REQUIRES_RECENT_LOGIN":
-                                    Toast.makeText(RegisterActivity.this, "This operation is sensitive and requires recent authentication. Log in again before retrying this request.", Toast.LENGTH_LONG).show();
-                                    break;
-
-                                case "ERROR_ACCOUNT_EXISTS_WITH_DIFFERENT_CREDENTIAL":
-                                    Toast.makeText(RegisterActivity.this, "An account already exists with the same email address but different sign-in credentials. Sign in using a provider associated with this email address.", Toast.LENGTH_LONG).show();
-                                    break;
-
-                                case "ERROR_EMAIL_ALREADY_IN_USE":
-                                    Toast.makeText(RegisterActivity.this, "The email address is already in use by another account.   ", Toast.LENGTH_LONG).show();
-                                    passwordTextInputSU.setError("The email address is already in use by another account.");
-                                    passwordEditTextSU.requestFocus();
-                                    break;
-
-                                case "ERROR_CREDENTIAL_ALREADY_IN_USE":
-                                    Toast.makeText(RegisterActivity.this, "This credential is already associated with a different user account.", Toast.LENGTH_LONG).show();
-                                    break;
-
-                                case "ERROR_USER_DISABLED":
-                                    Toast.makeText(RegisterActivity.this, "The user account has been disabled by an administrator.", Toast.LENGTH_LONG).show();
-                                    break;
-
-                                case "ERROR_USER_TOKEN_EXPIRED":
-                                    Toast.makeText(RegisterActivity.this, "The user\\'s credential is no longer valid. The user must sign in again.", Toast.LENGTH_LONG).show();
-                                    break;
-
-                                case "ERROR_USER_NOT_FOUND":
-                                    Toast.makeText(RegisterActivity.this, "There is no user record corresponding to this identifier. The user may have been deleted.", Toast.LENGTH_LONG).show();
-                                    break;
-
-                                case "ERROR_INVALID_USER_TOKEN":
-                                    Toast.makeText(RegisterActivity.this, "The user\\'s credential is no longer valid. The user must sign in again.", Toast.LENGTH_LONG).show();
-                                    break;
-
-                                case "ERROR_OPERATION_NOT_ALLOWED":
-                                    Toast.makeText(RegisterActivity.this, "This operation is not allowed. You must enable this service in the console.", Toast.LENGTH_LONG).show();
-                                    break;
-
-                                case "ERROR_WEAK_PASSWORD":
-                                    Toast.makeText(RegisterActivity.this, "The given password is invalid.", Toast.LENGTH_LONG).show();
-                                    passwordTextInputSU.setError("The password is invalid it must 6 characters at least");
-                                    passwordEditTextSU.requestFocus();
-                                    break;
-
-                            }
-                        }*/
     }
 
     private void initUi() // ánh xạ
@@ -189,8 +117,65 @@ public class RegisterActivity extends AppCompatActivity {
         passwordTextInputSU =  findViewById(R.id.passwordSU_text_input);
         passwordEditTextSU = findViewById(R.id.passwordSU_edit_text);
         btnSignUp =  findViewById(R.id.btnSU);
+        progressBar = findViewById(R.id.progressBar);
         ToLogPage =  findViewById(R.id.tvToLogin);
 
+    }
+
+    private void deleteErrorShowN()
+    {
+        emailEditTextSU.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View view, int i, KeyEvent keyEvent) {
+                String name = emailEditTextSU.getText().toString().trim();
+                if(TextUtils.isEmpty(name))
+                {
+                    nameTextInputSU.setError(null);
+                }
+                return false;
+            }
+        });
+    }
+
+    private void deleteErrorShowE()
+    {
+        emailEditTextSU.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View view, int i, KeyEvent keyEvent) {
+                String email = emailEditTextSU.getText().toString().trim();
+                if(Patterns.EMAIL_ADDRESS.matcher(email).matches())
+                {
+                    emailTextInputSU.setError(null);
+                }
+                return false;
+            }
+        });
+    }
+
+    private void deleteErrorShowP()
+    {
+        emailEditTextSU.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View view, int i, KeyEvent keyEvent) {
+                String pass = emailEditTextSU.getText().toString().trim();
+                if(pass.length() >=8)
+                {
+                    passwordTextInputSU.setError(null);
+                }
+                return false;
+            }
+        });
+    }
+
+    private void changeInProcess(boolean inProgress) // thanh progess và button
+    {
+        if(inProgress){
+            progressBar.setVisibility(View.VISIBLE);
+            btnSignUp.setVisibility(View.GONE);
+        } else {
+            progressBar.setVisibility(View.GONE);
+            btnSignUp.setVisibility(View.VISIBLE);
+        }
     }
 
     private void changeToLoginPage() //Sang trang đăng nhập
